cd25ad0219514cf0be6a80689725bc8e9a6b1fa2
gonzalorube <gonzalorube@gmail.com>
2019-09-29 04:26:43 -0300

    added js to get json and put it in the html as a list

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -1,7 +1,24 @@
-function mostrarData(data){
-    console.log(data);
-}
-
-$.get("localhost:8080/api/games").done(function(data){
-mostrarData(data);
-})
+    var games = document.getElementById("lista");
+    var gamesJson = $.getJSON( "/api/games").done(function recurse(data) {
+        console.log( "Success" );
+        var lista;
+        for (let i=0;i<data.length;i++){
+            lista += "<li>" + "<ul>" + "<li>" + "Game Id: " + data[i].id + "</li>" + "<li>" + "Created: " + data[i].created + "</li>" + "<li>" + "GamePlayers: " + "<ol>";
+            for(let e=0; e<data[i].gamePlayers.length; e++){
+                var gPlayer = data[i].gamePlayers[e];
+                console.log(gPlayer);
+                lista += "<li>" + "GamePlayer Id: " + data[i].gamePlayers[e].id + "</li>" + "<ul>" + "<li>" + "Player: " + "<ul>" + "<li>" + "Id: " + gPlayer.player.id + "</li>" + "<li>" + "Email: " + gPlayer.player.email + "</li>" + "</ul>" + "</li>" + "</ul>";
+            }
+            lista += "</ol>" + "</li>" + "</ul>" + "</li>";
+        }
+        games.innerHTML += lista;
+        console.log("I can't stop succeeding...")
+      })
+      .fail(function() {
+        console.log( "Salió todo mal, ¡RAJEMOS!" );
+      })
+      .always(function() {
+        console.log( "FUCK YEAH!");
+        console.log( "Completed." );
+        console.log( "I deserve a 'mate amargo'" );
+      });
