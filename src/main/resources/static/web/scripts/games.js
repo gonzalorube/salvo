f9e710230af05f8ed8d1f751241712b4c7eb7c49
gonzalo_rube <gonzalorube@gmail.com>
2019-10-28 21:54:12 -0300

    added stuff

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -1,14 +1,22 @@
-    var leaderboard = document.getElementById("leaderboard");
+
     // Obtengo la lista ordenada de id="lista"
-    var games = document.getElementById("lista");
+    // var games = document.getElementById("lista");
     // Solicito el JSON y, cuando es un éxito, lo itero y voy creando los elementos de lista
-    var gamesJson = $.getJSON( "/api/games").done(function recurse(data) {
-        leaderboard += "<tr>" + "<th>" + "Name" + "</th>" + "<th>" + "Win" + "</th>" + "<th>" + "Tie" + "</th>" + "<th>" + "Lost" + "</th>" + "</tr>";
+    var gamesJson = $.getJSON("/api/games").done(function recurse(data) {
+        console.log("Eh, eh");
+        var uName;
+        var table;
+        table = "<table><tr><th>Name</th><th>Total</th><th>Win</th><th>Tie</th><th>Lost</th></tr>";
         for(let k=0; k<data.length; k++){
-            leaderboard += "<th>"
+            console.log("pip");
+            for(let j; j<data[k].gamePlayers.length; j++){
+                console.log("pepe");
+            }
         }
+        console.log("HEY");
+        var leaderboard = document.getElementById("leaderboard").innerHTML = table;
         console.log( "Success" );
-        var lista ="";
+        /*var lista ="";
         for(let i=0; i<data.length; i++){
             lista += "<li>" + "<ul>" + "<li>" + "Game Id: " + data[i].id + "</li>" + "<li>" + "Created: " + data[i].created + "</li>" + "<li>" + "GamePlayers: " + "<ol>";
             for(let e=0; e<data[i].gamePlayers.length; e++){
@@ -20,7 +28,7 @@
         }
         // Envío la cadena guardada en "lista" al HTML de la lista ordenada accedida al principio
         games.innerHTML += lista;
-        console.log("I can't stop succeeding...");
+        console.log("I can't stop succeeding...");*/
       })
       .fail(function() { /* Por si falla... */
         console.log( "Salió todo mal, ¡RAJEMOS!" );
