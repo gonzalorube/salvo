effcc91977f52ef9be212b905e0a10ad827ac452
gonzalo_rube <gonzalorube@gmail.com>
2019-10-30 20:13:38 -0300

    commented code

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -3,7 +3,6 @@
     // var games = document.getElementById("lista");
     // Solicito el JSON y, cuando es un éxito, lo itero y voy creando los elementos de lista
     var gamesJson = $.getJSON("/api/games").done(function recurse(data) {
-        console.log("Eh, eh");
         var table;
         table = "<tr><th>Name</th><th>Total</th><th>Win</th><th>Tie</th><th>Lost</th></tr>";
         var uName = [];
@@ -15,34 +14,18 @@
                 } else { console.log("Already Exists"); }
             }
         }
-        console.log("HEY");
+
         var lBoard = document.getElementById("leaderboard").innerHTML = table;
         console.log( "Success" );
-        /*var lista ="";
-        for(let i=0; i<data.length; i++){
-            lista += "<li>" + "<ul>" + "<li>" + "Game Id: " + data[i].id + "</li>" + "<li>" + "Created: " + data[i].created + "</li>" + "<li>" + "GamePlayers: " + "<ol>";
-            for(let e=0; e<data[i].gamePlayers.length; e++){
-                var gPlayer = data[i].gamePlayers[e];
-                console.log(gPlayer);
-                lista += "<li>" + "GamePlayer Id: " + data[i].gamePlayers[e].id + "</li>" + "<ul>" + "<li>" + "Player: " + "<ul>" + "<li>" + "Id: " + gPlayer.player.id + "</li>" + "<li>" + "Email: " + gPlayer.player.email + "</li>" + "</ul>" + "</li>" + "</ul>";
-            }
-            lista += "</ol>" + "</li>" + "</ul>" + "</li>";
-        }
-        // Envío la cadena guardada en "lista" al HTML de la lista ordenada accedida al principio
-        games.innerHTML += lista;
-        console.log("I can't stop succeeding...");*/
 
         function getPointsPerPlayer(player2Compare){
                   var pTotal = 0.0;
-                  for(let k=0; k<data.length; k++){  // Itero el JSON de api7/games
-                      console.log("llegué!");
+                  for(let k=0; k<data.length; k++){  // Itero el JSON de api/games
                       for(let h=0; h<data[k].gamePlayers.length; h++){  // Itero cada gamePlayer
-                          console.log("cada vez más cerca!");
                           var player = data[k].gamePlayers[h].player.email;
                           console.log(player);
                           console.log(player2Compare + " por parámetro");
                           if(player === player2Compare){
-                              console.log("la pucha!");
                               if(data[k].gamePlayers[h].player.scores != null){
                                   pTotal += parseFloat(data[k].gamePlayers[h].player.scores);
                                   console.log(pTotal);
@@ -56,14 +39,12 @@
               function getWins(player2Compare){
                   var win = 0;
                   for(let k=0; k<data.length; k++){  // Itero el JSON de api7/games
-                      console.log("llegué!");
                       for(let h=0; h<data[k].gamePlayers.length; h++){  // Itero cada gamePlayer
-                          console.log("cada vez más cerca!");
                           var player = data[k].gamePlayers[h].player.email;
                           console.log(player);
                           console.log(player2Compare + " por parámetro");
                           var points = data[k].gamePlayers[h].player.scores;
-                          if(player === player2Compare && points != null && points == "1.0"){
+                          if(player === player2Compare && points != null && points == "1.0"){ // Mismo jugador? Puntos igual a 1? Sumale 1 al win
                               win++;
                               console.log(win);
                           }
@@ -75,9 +56,7 @@
               function getTieds(player2Compare){
                   var tied = 0;
                   for(let k=0; k<data.length; k++){  // Itero el JSON de api7/games
-                      console.log("llegué!");
                       for(let h=0; h<data[k].gamePlayers.length; h++){  // Itero cada gamePlayer
-                          console.log("cada vez más cerca!");
                            var player = data[k].gamePlayers[h].player.email;
                            console.log(player);
                            console.log(player2Compare + " por parámetro");
@@ -96,22 +75,34 @@
               (player2Compare){
                   var lost = 0;
                   for(let k=0; k<data.length; k++){  // Itero el JSON de api7/games
-                      console.log("llegué!");
                       for(let h=0; h<data[k].gamePlayers.length; h++){  // Itero cada gamePlayer
-                          console.log("cada vez más cerca!");
                           var player = data[k].gamePlayers[h].player.email;
                           console.log(player);
                           console.log(player2Compare + " por parámetro");
                           var points = data[k].gamePlayers[h].player.scores;
                           if(player === player2Compare && points != null && points == "0.0"){
                               lost++;
-                              console.log(lost
-                              );
+                              console.log(lost);
                           }
                       }
                   }
                   return lost;
               }
+
+              /*var lista ="";
+                      for(let i=0; i<data.length; i++){
+                          lista += "<li>" + "<ul>" + "<li>" + "Game Id: " + data[i].id + "</li>" + "<li>" + "Created: " + data[i].created + "</li>" + "<li>" + "GamePlayers: " + "<ol>";
+                          for(let e=0; e<data[i].gamePlayers.length; e++){
+                              var gPlayer = data[i].gamePlayers[e];
+                              console.log(gPlayer);
+                              lista += "<li>" + "GamePlayer Id: " + data[i].gamePlayers[e].id + "</li>" + "<ul>" + "<li>" + "Player: " + "<ul>" + "<li>" + "Id: " + gPlayer.player.id + "</li>" + "<li>" + "Email: " + gPlayer.player.email + "</li>" + "</ul>" + "</li>" + "</ul>";
+                          }
+                          lista += "</ol>" + "</li>" + "</ul>" + "</li>";
+                      }
+                      // Envío la cadena guardada en "lista" al HTML de la lista ordenada accedida al principio
+                      games.innerHTML += lista;
+                      console.log("I can't stop succeeding...");*/
+
       })
       .fail(function() { /* Por si falla... */
         console.log( "Salió todo mal, ¡RAJEMOS!" );
